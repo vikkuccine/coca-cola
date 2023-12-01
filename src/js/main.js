989ad7400827f0f3d192eeb1fe3c6f5bdb284295
vikkuccine <kamenkova09@gmail.com>
2023-12-02 00:45:02 +0200

    burger menu, shining stars

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
 generateSnow();
 showSnow(true);
+
+
+const burgerMenu = document.querySelector('.burger-menu');
+const menu = document.querySelector('.menu__wrap');
+const menuItem = document.querySelectorAll('.menu__item');
+
+burgerMenu.addEventListener('click', ()=> {
+    menu.classList.toggle('menu__wrap--open')
+    menuItem.forEach(item =>{
+        item.classList.toggle('menu__item--fade')
+    })
+})
