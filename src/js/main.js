196c4560b4a9a8b7eea3d75c3f6b5a61f8ee6560
vikkuccine <kamenkova09@gmail.com>
2024-01-02 13:47:58 +0200

    first screen desktop

--- src/js/main.js
+++ src/js/main.js
@@ -59,106 +59,24 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 
-function pageScroll() {
-	"use strict";
-	var pnls = document.querySelectorAll('.panel').length,
-		scdir, hold = false;
-
-	function _scrollY(obj) {
-		var slength, plength, pan, step = 100,
-			vh = window.innerHeight / 100,
-			vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
-		if ((this !== undefined && this.id === 'container') || (obj !== undefined && obj.id === 'container')) {
-			pan = this || obj;
-			plength = parseInt(pan.offsetHeight / vh);
+document.addEventListener("DOMContentLoaded", function () {
+	var scrollLinks = document.querySelectorAll('.scroll-item');
+  
+	scrollLinks.forEach(function (scrollLink) {
+	  scrollLink.addEventListener('click', function (e) {
+		e.preventDefault();
+  
+		var targetId = this.getAttribute('id');
+		var targetElement = document.getElementById(targetId);
+  
+		if (targetElement) {
+		  var offset = targetElement.offsetTop;
+  
+		  window.scrollTo({
+			top: offset,
+			behavior: 'smooth'
+		  });
 		}
-		if (pan === undefined) {
-			return;
-		}
-		plength = plength || parseInt(pan.offsetHeight / vmin);
-		slength = parseInt(pan.style.transform.replace('translateY(', ''));
-		if (scdir === 'up' && Math.abs(slength) < (plength - plength / pnls)) {
-			slength = slength - step;
-		} else if (scdir === 'down' && slength < 0) {
-			slength = slength + step;
-		} else if (scdir === 'top') {
-			slength = 0;
-		}
-		if (hold === false) {
-			hold = true;
-			pan.style.transform = 'translateY(' + slength + 'vh)';
-			setTimeout(function() {
-				hold = false;
-			}, 1000);
-		}
-	}
-
-	function _swipe(obj) {
-		var swdir,
-			sX,
-			sY,
-			dX,
-			dY,
-			threshold = 100,
-			/*[min distance traveled to be considered swipe]*/
-			slack = 50,
-			/*[max distance allowed at the same time in perpendicular direction]*/
-			alT = 500,
-			/*[max time allowed to travel that distance]*/
-			elT, /*[elapsed time]*/
-			stT; /*[start time]*/
-		obj.addEventListener('touchstart', function(e) {
-			var tchs = e.changedTouches[0];
-			swdir = 'none';
-			sX = tchs.pageX;
-			sY = tchs.pageY;
-			stT = new Date().getTime();
-		}, false);
-
-		obj.addEventListener('touchmove', function(e) {
-			e.preventDefault();
-		}, false);
-
-		obj.addEventListener('touchend', function(e) {
-			var tchs = e.changedTouches[0];
-			dX = tchs.pageX - sX;
-			dY = tchs.pageY - sY;
-			elT = new Date().getTime() - stT;
-			if (elT <= alT) {
-				if (Math.abs(dX) >= threshold && Math.abs(dY) <= slack) {
-					swdir = (dX < 0) ? 'left' : 'right';
-				} else if (Math.abs(dY) >= threshold && Math.abs(dX) <= slack) {
-					swdir = (dY < 0) ? 'up' : 'down';
-				}
-				if (obj.id === 'container') {
-					if (swdir === 'up') {
-						scdir = swdir;
-						_scrollY(obj);
-					} else if (swdir === 'down' && obj.style.transform !== 'translateY(0)') {
-						scdir = swdir;
-						_scrollY(obj);
-
-					}
-					e.stopPropagation();
-				}
-			}
-		}, false);
-	}
-
-	var container = document.getElementById('container');
-	container.style.transform = 'translateY(0)';
-	container.addEventListener('wheel', function(e) {
-		if (e.deltaY < 0) {
-			scdir = 'down';
-		}
-		if (e.deltaY > 0) {
-			scdir = 'up';
-		}
-		e.stopPropagation();
+	  });
 	});
-	container.addEventListener('wheel', _scrollY);
-	_swipe(container);
-	
-};
-
-pageScroll();
+  });
